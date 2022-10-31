7b8769903f13ab95461619d7f9f4f93aa88383b0
Kunal Dhongade <74715881+kunaldhongade@users.noreply.github.com>
2022-10-31 22:31:39 +0530

    updating Sidebar and improve durability

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,7 +1,8 @@
-import { DashboardRounded } from "@mui/icons-material";
 import React from "react";
+import { DashboardRounded } from "@mui/icons-material";
 import { motion } from "framer-motion";
 import "./Item.css";
+
 const Item = ({ icon, name }) => {
   const subheading = {
     true: {
@@ -13,24 +14,26 @@ const Item = ({ icon, name }) => {
     },
   };
   return (
-    <motion.div
-      className="item"
-      whileHover={{
-        backgroundColor: "rgba(255, 255, 255, 0.3)",
-        boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
-        backdropFilter: "blur(5.5px)",
-        WebkitBackdropFilter: "blur(5.5px)",
-        border: "1px solid rgba( 255, 255, 255, 0.18 )",
-        cursor: "pointer",
-      }}
-      transition={{
-        type: "none",
-        duration: 0.1,
-      }}
-    >
-      <motion.div className="icon">{icon}</motion.div>
-      <motion.span variants={subheading}>{name}</motion.span>
-    </motion.div>
+    <>
+     <motion.div
+       className="item"
+       whileHover={{
+         backgroundColor: "rgba(255, 255, 255, 0.3)",
+         boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+         backdropFilter: "blur(5.5px)",
+         WebkitBackdropFilter: "blur(5.5px)",
+         border: "1px solid rgba( 255, 255, 255, 0.18 )",
+         cursor: "pointer",
+       }}
+       transition={{
+         type: "none",
+         duration: 0.1,
+       }}
+     >
+       <motion.div className="icon">{icon}</motion.div>
+       <motion.span variants={subheading}>{name}</motion.span>
+     </motion.div>
+    </>
   );
 };
 
